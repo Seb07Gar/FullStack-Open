724634c24cb2eef81788eedc507444f0c50f9e9c
Sebastian Garzon <sebastiangarzon.2002@gmail.com>
2025-12-26 17:25:11 -0500

    Part 3 complete. ESlint added

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -17,9 +17,9 @@ morgan.token('body', (request) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-      response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response, next) => {
@@ -51,24 +51,24 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 
 app.delete('/api/persons/:id', (request, response, next) => {
-  
+
   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-  
-  const {name, number} = request.body
 
-  const person = new Person({name, number})
+  const { name, number } = request.body
+
+  const person = new Person({ name, number })
 
   person.save().then(savedPerson => {
     response.status(201).json(savedPerson)
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -79,7 +79,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     id,
     { name, number },
     {
-      new: true,          
+      new: true,
       runValidators: true,
       context: 'query'
     }
@@ -98,7 +98,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError'){
-    return response.status(400).json({error: error.message})
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -108,5 +108,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
